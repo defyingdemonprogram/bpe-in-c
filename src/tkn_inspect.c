461626c59245b7e8b7b68a5a6d4dd4496a9a1839
realsanjeev <realsanjeev2@gmail.com>
2026-02-10 21:51:48 +0545

    Added the limit to what to display in the token inspect, so it doesnot spill in the whole terminal

--- src/tkn_inspect.c
+++ src/tkn_inspect.c
@@ -14,6 +14,7 @@ int main(int argc, char **argv) {
     bool *ids       = flag_bool("ids", false, "Print the ids of the tokens");
     bool *render    = flag_bool("render", false, "Render tokens as text back");
     bool *split     = flag_bool("split", false, "When render is enabled, split tokens with bars |");
+    uint64_t *limit = flag_uint64("limit", 0, "Limit the amount of tokens to inspect (0 for no limit)");
     bool *help      = flag_bool("help", false, "Print this help");
 
     if (!flag_parse(argc, argv)) {
@@ -47,7 +48,7 @@ int main(int argc, char **argv) {
     
     for (size_t i = 0; i < tokens.count; ++i) {
         if (tokens.items[i] >= pairs.count) {
-            fprintf(stderr, "ERROR: token %zu: value %u is out of bounds (%zu)", i, tokens.items[i], pairs.count - 1);
+            fprintf(stderr, "ERROR: token %zu: value %u is out of bounds (%zu)\n", i, tokens.items[i], pairs.count - 1);
             return 1;
         }
     }
@@ -57,16 +58,24 @@ int main(int argc, char **argv) {
 
     if (*ids) {
         printf("------------------------------\n");
-        for (size_t i = 0; i < tokens.count; ++i) {
+        size_t n = tokens.count;
+        if (*limit > 0 && *limit < n) n = *limit;
+        for (size_t i = 0; i < n; ++i) {
             printf("%u\n", tokens.items[i]);
         }
     }
 
     if (*render) {
         printf("------------------------------\n");
-        for (size_t i = 0; i < tokens.count; ++i) {
+        String_Builder sb_raw = {0};
+        size_t n = tokens.count;
+        if (*limit > 0 && *limit < n) n = *limit;
+        for (size_t i = 0; i < n; ++i) {
+            sb_raw.count = 0;
+            render_token(pairs, tokens.items[i], &sb_raw);
+
             sb.count = 0;
-            render_token(pairs, tokens.items[i], &sb);
+            c_strlit_escape_bytes(sb_raw.items, sb_raw.count, &sb);
             sb_append_null(&sb);
             printf("%s", sb.items);
             if (*split) printf("|");
